95c67e4c58f2889513f0831e0aef1157bd7de6a1
Leon Rodriguez <leonrodriguez71@gmail.com>
2020-11-26 13:26:11 -0300

    ronaldo

--- crear_autitos.js
+++ crear_autitos.js
@@ -7,14 +7,15 @@ function crearAutos(marca, modelo, annio, precio){
 var autos = [];
 var cantidad_autos = prompt("cuantos autos queres?");
 
-for ( var i = 0 ; i < cantidad_autos ; i++){
+for (var i = 0 ; i < cantidad_autos ; i++){
     var marca = prompt("cual es la marca?");
     var modelo = prompt("cual es el modelo?");
     var annio = prompt("cual es el año de fabricacion?");
     var precio = prompt("cual es el precio?");
-    autos.push(new auto(marca, modelo, annio, precio));
+    autos.push(new crearAutos(marca, modelo, annio, precio));
 }
 
+console.log(autos);
 /*
 Vos armaste una funcion constructora crearAutos asi que deberias hacer
 new crearAutos(...) en lugar de new auto(...)
@@ -22,3 +23,5 @@ Yo usaria 'auto' como nombre de la funcion ya que te devuelve un auto
 Con ese cambio ya anda el loop, despues tendrias que ver que hacer
 con tu array autos, pero en principio podes poner un console.log(autos)
 */
+
+// los ubo aunque tampoco le agregué nada nuevo
